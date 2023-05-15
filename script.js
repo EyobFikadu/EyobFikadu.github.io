dab2555e8bd48371622d93fb4f79863d82138822
EyobFikadu <efikadu387@gmail.com>
2023-05-15 08:36:52 -0700

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+document.getElementById("Education").addEventListener("click", function () {
+  eyob();
+});
+
+function eyob() {
+  document.getElementById("jobaw").innerHTML =
+    "My name is Eyob fikadu <b/> I was born in 2000 Gc in addiss ababa. When i come up on to go to school i start my Elementary class at sunny side school which is found in addiss ababa<b/>After that i finish my High school and preparatory school at Abiyot kirs preparatory school<b/> Then now i am studing an Engineering field at bahirdar university ";
+}
+document.getElementById("Biography").addEventListener("click", function () {
+  eyoba();
+});
+
+function eyoba() {
+  document.getElementById("job").innerHTML =
+    "I don't know why it is but i am very passionated and interested when i am doing a code i think this is my passion that god gave me to profit with it .Althogh i am not a software engineering student ,i am tring my best to do different kinds of codes like HTML,CSS,Javascript,Bootstrap.And my dram is to be a best software developer in our country and make a great impact on Artificial intillegence";
+}
